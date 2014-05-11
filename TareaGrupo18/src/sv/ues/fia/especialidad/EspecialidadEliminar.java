6d7d27a86849d15e2f00aaf12bffaa014596e5c9
chali <chalij_hotmail.com>
2014-05-11 00:12:23 -0600

    actualizacion 4

--- TareaGrupo18/src/sv/ues/fia/especialidad/EspecialidadEliminar.java
+++ TareaGrupo18/src/sv/ues/fia/especialidad/EspecialidadEliminar.java
@@ -1,25 +1,35 @@
 package sv.ues.fia.especialidad;
 
 import sv.ues.fia.R;
-import sv.ues.fia.R.layout;
-import sv.ues.fia.R.menu;
+import sv.ues.fia.ControladorBDG18;
+import sv.ues.fia.institucion.Institucion;
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class EspecialidadEliminar extends Activity {
+	EditText editECInst;
+	ControladorBDG18 controlhelper;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_especialidad_eliminar);
+		controlhelper=new ControladorBDG18 (this);
+		editECInst=(EditText)findViewById(R.id.editECInstitucion);
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.especialidad_eliminar, menu);
-		return true;
+	public void eliminarEspecialidad(View v)
+	{
+		String regEliminadas;
+		Institucion institucion=new Institucion();
+		institucion.setIdindtitucion(Integer.parseInt(editECInst.getText().toString()));
+		controlhelper.abrir();
+		regEliminadas=controlhelper.eliminar(institucion);
+		controlhelper.cerrar();
+		Toast.makeText(this, regEliminadas, Toast.LENGTH_SHORT).show();
 	}
 
 }
