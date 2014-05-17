82ddf1aa416ef215a21a1c522d3cebf99f4224e9
stanleyvladimir2005 <stanleyvladimir2005@hotmail.com>
2014-05-17 17:44:00 -0600

    Subiendo clases facultad, perfil

--- TareaGrupo18/src/sv/ues/fia/facultad/Facultad.java
+++ TareaGrupo18/src/sv/ues/fia/facultad/Facultad.java
@@ -2,7 +2,32 @@ package sv.ues.fia.facultad;
 
 public class Facultad 
 {
+ private String IDfacultad="";
+ private String NombFacultad="";
 
+ public Facultad() {}
 
+public Facultad(String iDfacultad, String nombFacultad) {
+	super();
+	IDfacultad = iDfacultad;
+	NombFacultad = nombFacultad;
+}
+
+public String getIDfacultad() {
+	return IDfacultad;
+}
+
+public void setIDfacultad(String iDfacultad) {
+	IDfacultad = iDfacultad;
+}
+
+public String getNombFacultad() {
+	return NombFacultad;
+}
+
+public void setNombFacultad(String nombFacultad) {
+	NombFacultad = nombFacultad;
+}
+ 
 
 }
--- TareaGrupo18/src/sv/ues/fia/perfil/Perfil.java
+++ TareaGrupo18/src/sv/ues/fia/perfil/Perfil.java
@@ -2,5 +2,40 @@ package sv.ues.fia.perfil;
 
 public class Perfil 
 {
+	private String Nperfil="";
+	private String Estado="";
+	private String Observaciones="";
+	
+	public Perfil() {}
 
+	public Perfil(String nperfil, String estado, String observaciones) {
+		Nperfil = nperfil;
+		Estado = estado;
+		Observaciones = observaciones;
+	}
+
+	public String getNperfil() {
+		return Nperfil;
+	}
+
+	public void setNperfil(String nperfil) {
+		Nperfil = nperfil;
+	}
+
+	public String getEstado() {
+		return Estado;
+	}
+
+	public void setEstado(String estado) {
+		Estado = estado;
+	}
+
+	public String getObservaciones() {
+		return Observaciones;
+	}
+
+	public void setObservaciones(String observaciones) {
+		Observaciones = observaciones;
+	}
+	
 }
